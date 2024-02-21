417a9e5de5c434f5788634e9fca50185abd95729
Naveen120700 <naveen9120700@gmail.com>
2024-02-21 15:44:55 +0530

    init

---
+++ currency_convertor/app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+#
+# # Create your models here.
+# from django.db import models
+#
+# class Currency(models.Model):
+#     code = models.CharField(max_length=10)
+#     name = models.CharField(max_length=100)
+#     rate = models.FloatField()
+#
+#     def __str__(self):
+#         return self.code
